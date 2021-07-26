a62eea86e6609e9077129658b919af2df115a39e
lbrown <lbrown@digital-logic.net>
2021-07-26 12:46:32 -0700

    Capitalize challenge added

---
+++ src/main/java/net/digitallogic/Capitalize.java
@@ -0,0 +1,40 @@
+package net.digitallogic;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+/**
+ * Capitalize the first letter in each word in the provided string.
+ * Examples
+ * Input: "can i help you?", Output: "Can I Help You?"
+ */
+public class Capitalize {
+
+	public static String capitalize(String str) {
+		String[] words = str.split(" ");
+
+		for (int i=0; i<words.length; ++i)
+			words[i] = words[i].substring(0,1).toUpperCase() + words[i].substring(1);
+
+		return String.join(" ", words);
+	}
+
+	public static String stringJoiner(String str) {
+		String[] words = str.split(" ");
+
+		StringJoiner joiner = new StringJoiner(" ");
+
+		for (String word:words)
+			joiner.add(word.substring(0,1).toUpperCase() + word.substring(1));
+
+		return joiner.toString();
+	}
+
+	public static String stream(String str) {
+		return Arrays.stream(str.split(" "))
+			.map(word -> word.substring(0,1).toUpperCase() + word.substring(1))
+			.collect(Collectors.joining(" "));
+	}
+}
