13709a36e559366081efe6d53403eb885dca08a5
lbrown <lbrown@digital-logic.net>
2021-07-26 12:19:48 -0700

    ReverseString challenge added

---
+++ src/main/java/net/digitallogic/ReverseString.java
@@ -0,0 +1,97 @@
+package net.digitallogic;
+
+import java.util.*;
+import java.util.stream.Collector;
+
+/**
+ * Write a function that will return the provided string
+ * with the letter in reverse order.
+ * Examples
+ * Input: "car", Output: "rac"
+ * Input: "dog", Output: "god"
+ * <p>
+ * Try to create more than one solution.
+ */
+public class ReverseString {
+
+	// Slow solution, string concatenation within a loop
+	// should be avoided.
+	public static String appendString(String str) {
+		String result = "";
+
+		for (char c:str.toCharArray())
+			result = c + result;
+
+		return result;
+	}
+
+	public static String stringArray(String str) {
+		String[] result = new String[str.length()];
+
+		for (int f=0, b=str.length()-1; f < result.length; ++f, --b)
+			result[f] = String.valueOf(str.charAt(b));
+
+		return String.join("", result);
+	}
+
+	public static String StringList(String str) {
+		List<String> result = Arrays.asList(str.split(""));
+		Collections.reverse(result);
+		return String.join("", result);
+	}
+
+	public static String stringJoiner(String str) {
+		StringJoiner joiner = new StringJoiner("");
+
+		for (int i=str.length()-1; i>=0; --i)
+			joiner.add(String.valueOf(str.charAt(i)));
+
+		return joiner.toString();
+	}
+
+	public static String stringBuilder(String str) {
+		return new StringBuilder(str)
+			.reverse()
+			.toString();
+	}
+
+	public static String stack(String str) {
+		Deque<String> result = new ArrayDeque<>();
+
+		for (char c:str.toCharArray())
+			result.push(String.valueOf(c));
+
+		return String.join("", result);
+	}
+
+	// Slow solution, similar to stringAppend solution
+	// Performs string concatenation within a loop, in this case
+	// the loop is the reduce function.
+	public static String streamAppend(String str) {
+		return Arrays.stream(str.split(""))
+			.reduce("", (s,c) -> c + s);
+	}
+
+	public static String streamOne(String str) {
+		return String.join("", Arrays.stream(str.split(""))
+			.collect(LinkedList<String>::new,
+				LinkedList<String>::offerFirst,
+				(l1, l2) -> l1.addAll(0, l2)
+			));
+	}
+
+	public static String streamTwo(String str) {
+		return Arrays.stream(str.split(""))
+			.collect(
+				Collector.of(
+					ArrayDeque<String>::new,
+					ArrayDeque<String>::offerFirst,
+					(a1, a2) -> {
+						a2.addAll(a1);
+						return a2;
+					},
+					arr -> String.join("", arr)
+				)
+			);
+	}
+}
