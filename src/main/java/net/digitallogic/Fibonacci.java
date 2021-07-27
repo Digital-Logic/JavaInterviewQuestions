555af3ba53625f2e739998e77199ecf53acbd493
lbrown <lbrown@digital-logic.net>
2021-07-27 08:53:43 -0700

    Fibonaccit challenge added.

---
+++ src/main/java/net/digitallogic/Fibonacci.java
@@ -0,0 +1,81 @@
+package net.digitallogic;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Write a function that will produce the n-th number within the fibonacci sequence.
+ * The fibonacci sequence is:
+ * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+ * The next number in the fibonacci sequence is the sum of the previous two numbers.
+ * fib(n) = fib(n-1) + fib(n-2)
+ *
+ * Examples
+ * fib(1) = 1
+ * fib(3) = 2
+ * fib(5) = 5
+ * fib(8) = 21
+ */
+
+public class Fibonacci {
+	public static long iterative(int n) {
+		long[] cache = new long[n+1];
+
+		cache[0]=0;
+		cache[1]=1;
+
+		for(int i=2; i<=n; ++i)
+			cache[i] = cache[i-1] + cache[i-2];
+
+		return cache[n];
+	}
+
+	public static long iterativeTwo(int n) {
+		long[] cache = {0,1};
+
+		for (int i=2; i<=n; ++i) {
+			cache[i%2] = cache[0] + cache[1];
+		}
+
+		return cache[n%2];
+	}
+
+	public static long recursive(int n) {
+		if (n<=1)
+			return n;
+		return recursive(n-1) + recursive(n-2);
+	}
+
+	public static long recursiveMemoized(int n) {
+		long[] memo = new long[n+1];
+		return recursiveMemoized(n, memo);
+	}
+	private static long recursiveMemoized(int n, long[] memo) {
+		if (n<=1)
+			return n;
+
+		if (memo[n] == 0)
+			memo[n] = recursiveMemoized(n-1, memo) + recursiveMemoized(n-2, memo);
+
+		return memo[n];
+	}
+
+
+	public static Function<Integer,Long> memoizedFib = memoizer(n -> {
+		if (n <= 1)
+			return (long) n;
+		return Fibonacci.memoizedFib.apply(n-1) + Fibonacci.memoizedFib.apply(n-2);
+	});
+
+	private static <T,R> Function<T,R> memoizer(Function<T,R> func) {
+		final Map<T,R> memo = new HashMap<>();
+
+		return (T t) -> {
+			if (!memo.containsKey(t))
+				memo.put(t, func.apply(t));
+
+			return memo.get(t);
+		};
+	}
+}
---
+++ src/test/java/net/digitallogic/FibonacciTest.java
@@ -0,0 +1,96 @@
+package net.digitallogic;
+
+import lombok.Value;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.lang.reflect.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+class FibonacciTest {
+	@Test
+	void AtLestOneSolutionProvided() {
+		assertThat(
+			findTestMethods()
+		).describedAs("No solutions found to test.")
+			.hasAtLeastOneElementOfType(Method.class);
+	}
+
+	@TestFactory
+	Stream<DynamicNode> dynamicTests() {
+		return Stream.concat(
+			findTestFields()
+				.map(field -> dynamicContainer(field.getName(), testCases()
+					.map(args -> dynamicTest("Fib of " + args.getN(), () -> {
+						try {
+							Function<Integer, Long> func = (Function<Integer, Long>) field.get(new Fibonacci());
+							assertThat(func.apply(args.getN())).isEqualTo(args.getExpected());
+						} catch (IllegalAccessException | ClassCastException ex) {
+							fail("Unable to test field: " + field.getName());
+						}
+					}))
+				)),
+			findTestMethods()
+				.map(m -> dynamicContainer(m.getName(), testCases()
+					.map(args -> dynamicTest("Fib of " + args.getN(), () -> {
+						long result = (long) m.invoke(new Fibonacci(), args.getN());
+						assertThat(result).isEqualTo(args.getExpected());
+					}))
+				))
+		);
+	}
+
+
+	Stream<Field> findTestFields() {
+		return stream(Fibonacci.class.getDeclaredFields())
+			.filter(field -> field.getAnnotation(Disabled.class) == null)
+			.filter(field -> {
+				Type[] types = ((ParameterizedType) field.getGenericType()).getActualTypeArguments();
+				return field.getType() == Function.class &&
+					types[0] == Integer.class &&
+					types[1] == Long.class;
+			});
+
+	}
+
+	Stream<Method> findTestMethods() {
+		return stream(Fibonacci.class.getDeclaredMethods())
+			.filter(m -> Modifier.isPublic(m.getModifiers()))
+			.filter(m -> m.getAnnotation(Disabled.class) == null)
+			.filter(m -> {
+				Class<?>[] args = m.getParameterTypes();
+				return args.length == 1 &&
+					args[0] == Integer.TYPE &&
+					m.getReturnType() == Long.TYPE;
+			});
+	}
+
+	@Value(staticConstructor = "of")
+	private static class Args {
+		int n;
+		long expected;
+	}
+
+	Stream<Args> testCases() {
+		return Stream.of(
+			Args.of(1, 1),
+			Args.of(2, 1),
+			Args.of(3, 2),
+			Args.of(4, 3),
+			Args.of(5, 5),
+			Args.of(6, 8),
+			Args.of(20, 6765),
+			Args.of(25, 75025),
+			Args.of(40, 102334155)
+		);
+	}
+}
