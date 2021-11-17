b2ceed64e7129c7e3856b94bea24c9e857232fe6
lbrown <lbrown@digital-logic.net>
2021-11-17 11:18:50 -0700

    Update Directions and Solutions

--- src/main/java/net/digitallogic/Anagrams.java
+++ src/main/java/net/digitallogic/Anagrams.java
@@ -4,11 +4,11 @@ package net.digitallogic;
  * Given two strings, write a function that will determine if one string is
  * an anagram of another. An anagram is a string that is formed by rearranging
  * the letters of another string.
- * Ignore capitalization, and spaces. Punctuation will not be included in the provided strings.
+ * Ignore capitalization, punctuation, and spaces.
  *
  * Examples
  *  isAnagram("Arc", "Car") => true
- *  isAnagram("Debit card", "Bad credit") => true
+ *  isAnagram("D e b i t card", "Bad credit") => true
  *  isAnagram("Astronomer!", "Moon starer") => true
  */
 public class Anagrams {
