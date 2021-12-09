9681fe33812d7e8c0c150cb84db9464a0fc4928f
lbrown <lbrown@digital-logic.net>
2021-12-09 16:13:01 -0700

    Updated instructions

--- src/main/java/net/digitallogic/MatrixRotation.java
+++ src/main/java/net/digitallogic/MatrixRotation.java
@@ -1,8 +1,8 @@
 package net.digitallogic;
 
 /**
- * 1. Write a function that will rotate an NxN matrix clockwise by 90 degrees.
- *    The matrix is guaranteed to be square.
+ * Write a function that will rotate an NxN matrix clockwise by 90 degrees.
+ * The matrix is guaranteed to be square.
  * Example
  * {{1,2}, => {{3,1},
  *  {3,4}}     {4,2}}
@@ -11,7 +11,7 @@ package net.digitallogic;
  *   {4,5,6}, =>  {8,5,2},
  *   {7,8,9}}     {9,6,3}}
  *
- *  2. Can you rotate the matrix in-place?
+ *  Extra Credit: Can you rotate the matrix in-place?
  **/
 
 public class MatrixRotation {
